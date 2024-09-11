709db113b940c882f304331c34dce8e84cf70884
Harshit Singla <harshitashwani@gmail.com>
2024-09-11 14:12:02 +0530

    Started creating Job listing page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './pages/signup';
 import Home from './pages/Home';  // Home or any other component
 import Login from './pages/LoginPage';  // Home or any other component
+import Jobs from './pages/jobListing';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/jobs" element={<Jobs />} />
         {/* Add other routes as needed */}
       </Routes>
     </Router>
---
+++ src/pages/jobListing.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import NavBar from '../navbar';
+import jobData from 'jobData.json'; // Assuming your jobData.json is in the same folder
+
+function Jobs() {
+    return (
+        <>
+            <NavBar />
+            <h3>Find Your Dream Job</h3>
+            {jobData.joblisting.map((job, index) => (
+                <div key={index}>
+                    <h4>{job.title}</h4>
+                    <h6>Company: {job.company}</h6>
+                    <h6>Category: {job.category}</h6>
+                    <h6>Salary: {job.salary}</h6>
+                    <h6>Location: {job.location}</h6>
+                    <h6>Job Type: {job.jobType}</h6>
+                    <p>Description: {job.description}</p>
+                    <p>Level: {job.level}</p>
+                    <div>
+                        <strong>Technical Skills:</strong>
+                        {job.technicalSkills.map((skill, i) => (
+                            <p key={i}>{skill}</p>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default Jobs;
